075cdb11d68e41627e4a27990e0f10ab6f9584c6
autocommit <autocommit>
2019-09-01 21:34:27 +0000

    auto 0 20 214

--- all_types.snippet.h
+++ all_types.snippet.h
@@ -1,39 +1,42 @@
 #ifndef DB_ALL_TYPES_SNIPPET_H
 #define DB_ALL_TYPES_SNIPPET_H
 #include "concatsym.h"
-#include <stdint.h> // uint64_t
+#include "myint.h"
 
 #define TYPE blob
-#define BIND_ARGS const void* val, int len, void(*)(void*)destructor
+#define BIND_ARGS const void* val, int len, void(*destructor)(void*) 
 #define BIND_PARAMS val, len, destructor
 #define COLUMN_RETURN const void*
 #include "types.snippet.h"
 #define TYPE blob64
 #undef BIND_ARGS
-#define BIND_ARGS const void* blob, uint64_t len, void(*)(void*)destructor
+#define BIND_ARGS const void* blob, u64 len, void(*destructor)(void*)
 #include "types.snippet.h"
 #define TYPE text
 #undef BIND_ARGS
-#define BIND_ARGS const char* val, int len, void(*)(void*)destructor
+#define BIND_ARGS const char* val, int len, void(*destructor)(void*)
 #undef COLUMN_RETURN
 #define COLUMN_RETURN const char*
 #include "types.snippet.h"
 #define TYPE text64
 #undef BIND_ARGS
-#define BIND_ARGS const char* val, uint64_t len, void(*)(void*)destructor
+#define BIND_ARGS const char* val, u64 len, void(*destructor)(void*)
 #include "types.snippet.h"
 #undef BIND_ARGS
 #define BIND_ARGS TYPE val
 #undef BIND_PARAMS
 #define BIND_PARAMS val
+#undef COLUMN_RETURN
 #define COLUMN_RETURN TYPE
 #define TYPE double
 #include "types.snippet.h"
 #define TYPE int
 #include "types.snippet.h"
 #define TYPE int64
+#undef COLUMN_RETURN
+#define COLUMN_RETURN s64
 #undef BIND_ARGS
-#define BIND_ARGS uint64_t val
+#define BIND_ARGS s64 val
 #include "types.snippet.h"
 #define TYPE zeroblob
 #undef BIND_ARGS
@@ -41,7 +44,7 @@
 #include "types.snippet.h"
 #define TYPE zeroblob64
 #undef BIND_ARGS
-#define BIND_ARGS uint64_t val
+#define BIND_ARGS s64 val
 #include "types.snippet.h"
 
 #undef BIND_ARGS
