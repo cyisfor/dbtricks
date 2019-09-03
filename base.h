89386488061e5553d484ffc1be0c040a20d12f69
user <email>
2019-09-03 01:25:45 +0000

    CPP macros suck

--- base.h
+++ base.h
@@ -78,6 +78,7 @@ bool N(has_table_str)(T db, string table_name);
 #include "all_types.snippet.h"
 result N(bind_string)(N(stmt) stmt, int col, string value);
 string N(column_string)(N(stmt) stmt, int col);
+#define basedb_bind_identifier basedb_bind_int64
 #define basedb_column_identifier basedb_column_int64
 
 #undef N
