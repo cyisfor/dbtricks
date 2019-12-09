365df863c408be1d5a3aacd061ba976a595d0e36
autocommit <autocommit>
2019-12-09 18:29:11 +0000

    auto 0 13 96

--- db/types.m4.h
+++ db/types.m4.h
@@ -1,10 +1,10 @@
-m4_ifdef(IMPLEMENTATION, {{
+m4_ifdef({{IMPLEMENTATION}}, {{m4_dnl;
 EXPORT
 result basedb_bind_{{}}TYPE(N(stmt) stmt, int col, BIND_ARGS) {
 	return check(stmt->db,
 				 sqlite3_bind_{{}}TYPE(stmt->sqlite, col, BIND_PARAMS));
 }
-m4_ifdef(COLUMN_RETURN, {{
+m4_ifdef({{COLUMN_RETURN}}, {{m4_dnl;
 EXPORT
 COLUMN_RETURN basedb_column_{{}}TYPE(N(stmt) stmt, int col) {
 	return sqlite3_column_{{}}TYPE(stmt->sqlite, col);
