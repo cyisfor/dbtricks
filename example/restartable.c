fe16c0bd782d7aa802361559d05ca1ef59e04cbc
autocommit <autocommit>
2019-12-14 17:12:16 +0000

    auto 0 17 206

--- example/restartable.c
+++ example/restartable.c
@@ -6,22 +6,22 @@
 
 
 static
-result bar_in_transaction(basedb db, basedb_stmt insert, int val, char val2) {
+result bar_in_transaction(basedb db, basedb_stmt insert, int i, int val, char val2) {
 	result cleanup(void) {
 		puts("cleanup for retrying");
+		sleep(1);
 		return result_busy;
 	}
 
 	basedb_bind_int(insert, 1, val);
 	basedb_bind_int(insert, 2, val2);
-	int i;
-	for(i=0;i<10;++i) {
-		basedb_bind_int(insert, 3, i);
-		/* XXX: check busy after every return? */
-		transdb_check(basedb_once(insert));
-		sleep(1);
-		printf("inserted %d\n", i);
-	}
+	basedb_bind_int(insert, 3, i);
+	/* XXX: check busy after every return? */
+	transdb_check(basedb_once(insert));
+	transdb_check(basedb_once(insert));
+	transdb_check(basedb_once(insert));
+	sleep(1);
+	printf("%d inserted %d\n", getpid(), i);
 }
 
 #include "foo.c"
@@ -32,9 +32,13 @@ int main(int argc, char *argv[])
 	basedb_exec(db, "CREATE TABLE IF NOT EXISTS foo (id INTEGER PRIMARY KEY, val INTEGER)");
 	basedb_stmt insert = basedb_prepare(db, "INSERT INTO foo (val) SELECT ?+?+?");
 	transdb trans = transdb_open(db);
-	bar(trans, DEFERRED_TRANSACTION,
-		insert, 23, 42);
-
+	int i;
+	for(i=0;i<10;++i) {
+		printf("trying %d\n", i);
+		bar(trans, DEFERRED_TRANSACTION,
+			insert, i, 23, 42);
+		sleep(1);
+	}
 	transdb_close(trans);
 	basedb_finalize(insert);
 	basedb_close(db);
